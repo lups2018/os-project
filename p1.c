11c573b82081be69b589379365c3ef53c722d840
lups2018 <37378039+lups2018@users.noreply.github.com>
2018-03-25 22:18:29 +0530

    Update p1.c

--- p1.c
+++ p1.c
@@ -9,8 +9,7 @@ void pop1();
 void pop2();
 int queue1[size],queue2[size],fronts=-1,rears=-1,frontt=-1,reart=-1;
 void enq1(int val1)
-{//int i;
- if(rears==size-1)
+{ if(rears==size-1)
   {printf("student queue is full:insertion not possible"); 
   }
 else
@@ -42,43 +41,47 @@ void pop1(){
    }
 }
 void pop2(){
-   if(frontt== reart)
+   if(frontt == reart)
       printf("no teacher avialable  Queue is Empty!\n");
    else{
-      printf("\nDeleted : %d\n", queue2[frontt]);
+      printf("\nDeleted : %d\n", queue1[frontt]);
       frontt++;
    }
 }
 
-void processed(){
-      int i;
-if(frontt!=reart)
-   {  for(i=frontt; i<=reart; i++)
-	 printf("%d\t",queue2[i]);
-      // if(fronts!=rears)
+void display(){
+   int i;
+   if(reart == -1)
+      {printf("\nno teacher in queue!!!\n");
+       printf("studnets\n");
        for(i=fronts; i<=rears; i++)
 	 printf("%d\t",queue1[i]);
-
-   } 
-else
-   {
-     for(i=fronts; i<=rears; i++)
-       {printf("%d\t",queue1[i]);
-       }
 }
+   else{
+      int i;
+      printf("teahcers\n");
+      for(i=frontt; i<=reart; i++)
+	 printf("%d\n",queue2[i]);
+      printf("students\n");
+      //if(rears == -1)
+       //{
+      for(i=fronts; i<=rears; i++)
+	 printf("%d\t",queue1[i]);
+      // }
+   }
 }
 
 int main()
 { int val1,val2,ch1,ch2,ch3;
   while(1)
-  { printf("*************menu****************\n");
+  { printf("*********menu***********\n");
     printf("press1: insert student or teacher\n");
-    printf("press2:  pop student or teacher\n");
+    printf("press2: pop student or teacher\n");
     printf("press3: to display\n");
     printf("enter your choice\n");
     scanf("%d",&ch1);
     switch(ch1)
-    { case 1:{
+    { case 1:
       printf("press1: to insert student\n");
       printf("press2: to insert teacher\n");
        scanf("%d",&ch2);  
@@ -94,9 +97,10 @@ int main()
          enq2(val2);
          break;
        }
-      }
+
       break;
-      case 2:{
+
+      case 2:
       printf("press1: to pop student\n");
       printf("press2: to pop teacher\n");
       scanf("%d",&ch3);
@@ -108,10 +112,9 @@ int main()
       pop2();
       break;
       }
-}
       break;
       case 3:
-      processed();
+      display();
       break;
       case 4:
       exit(0);
