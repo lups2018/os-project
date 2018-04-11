4652d4a9456ec922d18f530cec79cb033c41a180
lups2018 <37378039+lups2018@users.noreply.github.com>
2018-04-11 15:30:23 +0530

    Update p1.c
    added number of books taken by the students.

--- p1.c
+++ p1.c
@@ -1,23 +1,33 @@
 #include<stdlib.h>
 #include<stdio.h>
 #define size 10
-//#include<conio.h>
-void enq1(int);
+void enq1(int,int);
 void enq2(int);
 void display();
+void book(int);
 int queue1[size],queue2[size],fronts=-1,rears=-1,frontt=-1,reart=-1;
-void enq1(int val1)
-{ if(rears==size-1)
+int stud[10],b2[10];
+int count=5;
+void enq1(int val1,int c)
+{
+ if(rears==size-1)
   {printf("student queue is full:insertion not possible"); 
   }
 else
-{if(fronts==-1)
+{
+if(fronts==-1)
   fronts=0;
   rears++;
  queue1[rears]=val1;
- printf("students are inserted");
+ 
+ printf("students are inserted\n");
+//printf("enter book");
+stud[rears]=c;
+printf("books inserted are %d\n",stud[rears]);
 }
 }
+
+  
 void enq2(int val2)
 { if(reart==size-1)
   {printf("Teacher queue is full:insertion not possible\n"); 
@@ -34,70 +44,88 @@ void process(){
    if(reart == -1)
      {
       printf("no teacher in queue to process!\n");
-	while(fronts<=rears)
-	{
-      printf("\nProcessed : %d\n", queue1[fronts]);
+	if(rears == -1)
+       {
+         printf("no student in queue to process!\n");
+ 	} 
+        else{
+              printf("\nProcessed student: %d\n", queue1[fronts]);
       fronts++;
-	}
+      rears=-1;
+             } 
+      
    }
    else{
       while(frontt<=reart)
 	{
-      printf("\nProcessed : %d\n", queue2[frontt]);
+      printf("\nProcessed teacher: %d\n", queue2[frontt]);
       frontt++;
 	}
 	while(fronts<=rears)
 	{
-      printf("\nProcessed : %d\n", queue1[fronts]);
+      printf("\nProcessed student: %d\n", queue1[fronts]);
       fronts++;
 	}
    }
 }
 
 void display(){
-   int i;
+   int i,j;
    if(reart == -1)
       {printf("\nno teacher in queue!!!\n");
-       printf("studnets\n");
+       printf("students!!!books\n");
        for(i=fronts; i<=rears; i++)
-	 printf("%d\t",queue1[i]);
+       {
+	 printf("%d\n",queue1[i]);
+         printf("%d\n",stud[i]);
+        
+       }
+       
 }
    else{
       int i;
-      printf("teahcers\n");
+      printf("teahcers!!!\n");
       for(i=frontt; i<=reart; i++)
 	 printf("%d\n",queue2[i]);
-      printf("students\n");
-      //if(rears == -1)
-       //{
+      printf("students!!!\n");
       for(i=fronts; i<=rears; i++)
-	 printf("%d\t",queue1[i]);
-      // }
+	 printf("%d\n",queue1[i]);
    }
 }
 
 int main()
-{ int val1,val2,ch1,ch2,ch3;
+{ int val1,val2,ch1,ch2,ch3,c;
   while(1)
-  { printf("*********menu***********\n");
-    printf("press1: insert student or teacher\n");
-    printf("press2: pop student or teacher\n");
-    printf("press3: to display\n");
-    printf("enter your choice\n");
+  { 
+    printf("\tMenu for id's of Student and Teacher\n");
+    printf("\t\t|Student|Teacher|\n");
+    printf("\t\t|   1   |   2   |\n");
+    printf("\t\t|   3   |   4   |\n");
+    printf("\t\t|   5   |   6   |\n");
+    printf("\t\t|   7   |   8   |\n");
+    printf("\t\t|   9   |  10   |\n");
+    printf("\t\tMenu\n");
+    printf("1-press1: insert student or teacher on queue.\n");
+    printf("2-press2: process student or teacher on library counter.\n");
+    printf("3-press3: to display both the queues.\n");
+    printf("\t\tenter your choice\n");
     scanf("%d",&ch1);
     switch(ch1)
 
     { case 1:
-      printf("press1: to insert student\n");
-      printf("press2: to insert teacher\n");
+      printf("1-press1: to insert student\n");
+      printf("2-press2: to insert teacher\n");
        scanf("%d",&ch2);  
        switch(ch2)
       { case 1: 
          printf("enter the values to insert\n");
          scanf("%d",&val1);
-         enq1(val1);
+         //enq1(val1);
+         printf("enter the books\n");
+         scanf("%d",&c);
+         enq1(val1,c);
          break;
-        case 2:
+        case 2:// teacher
          printf("enter the values to insert\n");
          scanf("%d",&val2);
          enq2(val2);
